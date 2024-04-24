a9b6547c1fbcf74f81248c790f3f243608a1e43a
Juho Ervasti <juho.ervasti@gispo.fi>
2024-04-24 13:10:08 +0300

    Harmonize dev/prod repo

--- render.ps1
+++ render.ps1
@@ -24,23 +24,4 @@ else {
     }
 }
 
-
-docker run -it --rm -v "$(pwd)/src:/app" -v "$(pwd)/out:/out" -v "$(pwd)/docs/assets:/assets" -e COURSE=$arg1 gispo/bookdown:latest bash -c '
-    set -e
-    rm -rf /out/*
-
-    if [ "$COURSE" = "all" ]; then
-        courses=(/app/*/)
-    else
-        courses=("/app/$COURSE/")
-    fi
-
-    for course_dir in "${courses[@]}"; do
-        folder_name=$(basename "$course_dir")
-        mkdir /out/"$folder_name"
-        Rscript /app/knit.R "$course_dir"
-        mv "$course_dir"_book/* /out/"$folder_name"
-        rm -r "$course_dir"_book
-    done
-    chmod --recursive 777 /out
-'
+docker run --rm -v "${pwd}/src:/app" -v "${pwd}/out:/out" gispo/bookdown:latest /app/run_win.sh $arg1
